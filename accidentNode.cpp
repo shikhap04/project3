1753b5acac7c6baee7426c513b8346c82ffc4518
azure219 <45633746+azure219@users.noreply.github.com>
2024-04-17 18:38:59 +0000

    comment test

--- accidentNode.cpp
+++ accidentNode.cpp
@@ -43,6 +43,7 @@ accidentNode::accidentNode(int time, unsigned int day, unsigned int month, unsig
     this->weatherCond = weatherCond;
     this->planeCompany = planeCompany;
     this->planeModel = planeModel;
+    // comment
 }
     
 accidentNode::~accidentNode() {}
