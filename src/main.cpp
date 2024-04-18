0b9da54d156b4abbcc395fbfbfc0e7ac19169be5
shikhap04 <the.shikha.patel@gmail.com>
2024-04-18 02:15:08 -0400

    notes

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
+/*
+USER INTERFACE HERE
+- initialize program (make sure big vector is created once)
+- print to CLI options
+- read parameters
+- call search parameters in moa
+- (somehow gets small vector, probably return in search parametrs or sort parameters)
+- prints first x number of elements in small vector
+*/
 
 int main () {
 
--- src/moa.cpp
+++ src/moa.cpp
@@ -6,3 +6,16 @@
 #include <unordered_map>
 #include <iterator>
 
+using namespace std;
+
+/*
+program class
+- constructor parses and makes the vector of nodes
+- get search parameter (function called in main)
+    - maybe it stores parameters in the object itself (when get new parameters, it sets everything else to null/-1)
+- does search
+- then does sort
+    - heap sort
+    - quick sort
+- return small sorted vector to main
+*/
